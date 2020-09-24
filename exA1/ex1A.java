e7df71c12d2e090483113a6e192a3442e06158cb
Sonja De Meersman <sonja_dm@telenet.be>
2020-09-24 09:44:11 +0200

    ex1A

---
+++ exA1/ex1A.java
@@ -0,0 +1,11 @@
+public class ex1A {
+
+	public static void main(String[] args)  {
+		String courseTitle = "Java Reskilling";
+		int nbrDays = 20;
+		double pricePerDay = 195.99;
+		boolean priorKnowledgeRequired = false;
+		
+		System.out.println("Course title: " +courseTitle+ "\nNumber of days: " +nbrDays+ "\nPrice per day: " +pricePerDay+ "\nPrior knowledge requested : " +priorKnowledgeRequired);
+	}
+}
